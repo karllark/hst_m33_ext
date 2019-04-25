7137715b7eca433b46d3f17bd08e637ebc1ac0db
Karl Gordon <kgordon@stsci.edu>
2019-04-25 14:56:53 -0400

    New stuff

--- cutout_pcatalog.py
+++ cutout_pcatalog.py
@@ -43,9 +43,9 @@ def get_postage_catalog(radecstr,
 if __name__ == '__main__':
 
     # get the large fits file and wcs info
-    filenames = ['/home/kgordon/Hubble/M33Ext/14610_M33-B01_1.phot.fits.gz',
-                 '/home/kgordon/Hubble/M33Ext/14610_M33-B01_2.phot.fits.gz',
-                 '/home/kgordon/Hubble/M33Ext/merged-6filt-bricks23.fits.gz']
+    filenames = ['/home/kgordon/HST/M33-Ext/14610_M33-B01_1.phot.fits.gz',
+                 '/home/kgordon/HST/M33-Ext/14610_M33-B01_2.phot.fits.gz',
+                 '/home/kgordon/HST/M33-Ext/merged-6filt-bricks23.fits.gz']
     ftags = ['B01_2', 'B01_1', 'B23']
 
     # h, m, s have to be lower case for radecstr.  That is specific
@@ -67,6 +67,17 @@ if __name__ == '__main__':
                  '01h34m10.5900s +30d46m16.10s',
                  '01h33m34.2600s +30d33m27.60s']
 
+    # new set
+    starnames = ['J013341.93+304728_VX',
+                 'J013306.53+303010_VX',
+                 'J013340.10+304138_VX',
+                 'J013313.76+305240_VX']
+
+    radecstrs = ['01h33m41.93s +30d47m28.3s',
+                 '01h33m06.53s +30d20m10.8s',
+                 '01h33m40.10s +30d41m38.4s',
+                 '01h33m13.76s +30d52m40.1s']
+
     # size to extract in (ra, dec) arcsec
     size = (10./3600.)*u.degree
 
@@ -83,4 +94,5 @@ if __name__ == '__main__':
             print(pcat)
 
             if pcat is not None:
-                pcat.write('%s_%s_pcatalog.fits' % (starnames[i], ftags[k]))
+                pcat.write('%s_%s_pcatalog.fits' % (starnames[i], ftags[k]),
+                           overwrite=True)
--- cutout_pstamps.py
+++ cutout_pstamps.py
@@ -3,6 +3,7 @@ from astropy.coordinates import SkyCoord
 from astropy.io import fits
 from astropy.wcs import wcs
 from astropy.wcs.utils import proj_plane_pixel_scales
+from astropy.table import Table
 
 
 def get_postage_stamp(radecstr,
@@ -30,43 +31,66 @@ def get_postage_stamp(radecstr,
     pix_scales = 3600.*proj_plane_pixel_scales(image_wcs)
     pix_size = (size[0]/pix_scales[0], size[1]/pix_scales[1])
 
-    return Cutout2D(image, position, pix_size, wcs=image_wcs)
+    try:
+        result = Cutout2D(image, position, pix_size, wcs=image_wcs)
+    except:
+        return None
+
+    return result
 
 
 if __name__ == '__main__':
 
+    # h, m, s have to be lower case for radecstr.  That is specific
+    # maybe check in astropy why this is the case
+    # starnames = ['J013415.71+303341_V01',
+    #              'J013358.07+303308_V02',
+    #              'J013352.13+304319_V03',
+    #              'J013344.59+304436_V04',
+    #              'J013406.63+304147_V05',
+    #              'J013339.52+304540_V06',
+    #              'J013410.59+304616_V07',
+    #              'J013334.26+303327_V08']
+    # radecstrs = ['01h34m15.6800s +30d33m40.90s',
+    #             '01h33m58.0700s +30d33m08.20s',
+    #             '01h33m52.1300s +30d43m19.30s',
+    #             '01h33m44.5900s +30d44m36.90s',
+    #             '01h34m06.6300s +30d41m47.80s',
+    #             '01h33m39.5200s +30d45m40.50s',
+    #             '01h34m10.5900s +30d46m16.10s',
+    #             '01h33m34.2600s +30d33m27.60s']
+
+    # get the new set
+    a = Table.read('/home/kgordon/Python_git/hst_m33_ext/m33OB_fin.csv')
+
+    starnames = a['name'].data
+    radecstrs = []
+    for k in range(len(a)):
+        cstr = '{}h{}m{}s {}d{}m{}s'.format(a['rah'][k], a['ram'][k],
+                                            a['ras'][k], a['decd'][k],
+                                            a['decm'][k], a['decs'][k])
+        radecstrs.append(cstr)
+
+    print(starnames)
+    print(radecstrs)
+
     # get the large fits file and wcs info
     filename = 'F475W_mosaic.fits.gz'
+    cband = 'F475W'
     hdulist = fits.open(filename)
     image_wcs = wcs.WCS(hdulist[0].header)
     image = hdulist[0].data
-
-    # h, m, s have to be lower case for radecstr.  That is specific
-    # maybe check in astropy why this is the case
-    starnames = ['J013415.71+303341_V01',
-                 'J013358.07+303308_V02',
-                 'J013352.13+304319_V03',
-                 'J013344.59+304436_V04',
-                 'J013406.63+304147_V05',
-                 'J013339.52+304540_V06',
-                 'J013410.59+304616_V07',
-                 'J013334.26+303327_V08']
-    radecstrs = ['01h34m15.6800s +30d33m40.90s',
-                 '01h33m58.0700s +30d33m08.20s',
-                 '01h33m52.1300s +30d43m19.30s',
-                 '01h33m44.5900s +30d44m36.90s',
-                 '01h34m06.6300s +30d41m47.80s',
-                 '01h33m39.5200s +30d45m40.50s',
-                 '01h34m10.5900s +30d46m16.10s',
-                 '01h33m34.2600s +30d33m27.60s']
 
     # size to extract in (ra, dec) arcsec
     size = (10., 10.)
 
     for k, cradec in enumerate(radecstrs):
         pimage = get_postage_stamp(cradec, size, image, image_wcs)
-        print(pimage.wcs)
 
-        ohdu = fits.PrimaryHDU(pimage.data, header=pimage.wcs.to_header())
-        ohdul = fits.HDUList([ohdu])
-        ohdul.writeto('%s_pstamp.fits' % starnames[k], overwrite=True)
+        if pimage is not None:
+            print(pimage.wcs)
+
+            ohdu = fits.PrimaryHDU(pimage.data, header=pimage.wcs.to_header())
+            ohdul = fits.HDUList([ohdu])
+            ohdul.writeto('%s_%s_pstamp.fits' % (starnames[k], cband),
+                          overwrite=True)
